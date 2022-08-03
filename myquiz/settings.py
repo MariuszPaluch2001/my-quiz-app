f9e58e483d6d5f52b3e994c3d9b75877c8f1eaf7
MariuszPaluch2001 <mariuszpaluch001@gmail.com>
2022-08-03 20:34:43 +0200

    Add simple html.

--- myquiz/settings.py
+++ myquiz/settings.py
@@ -54,7 +54,7 @@ ROOT_URLCONF = 'myquiz.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': ['templates'],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
@@ -123,3 +123,5 @@ STATIC_URL = 'static/'
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
 django_on_heroku.settings(locals())
+
+
