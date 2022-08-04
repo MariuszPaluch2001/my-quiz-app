e7d2c8c35680b39a99bd7c7783fbce014d40b6e5
MariuszPaluch2001 <mariuszpaluch001@gmail.com>
2022-08-04 20:40:25 +0200

    Add static files suport.

--- app/urls.py
+++ app/urls.py
@@ -1,5 +1,11 @@
 from django.urls import path
 from app import views
+from django.contrib.staticfiles.urls import staticfiles_urlpatterns
 
 urlpatterns = [
+    path('', views.render_home, name="home"),
+    path('about', views.render_about, name="about"),
+    path('quiz_menu', views.render_quiz_menu, name="quiz_menu")
 ]
+
+urlpatterns += staticfiles_urlpatterns()
--- myquiz/urls.py
+++ myquiz/urls.py
@@ -14,11 +14,11 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import path
-from app import views
+from django.urls import path, include
+from django.contrib.staticfiles.urls import staticfiles_urlpatterns
+
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('', views.render_home, name="home"),
-    path('about', views.render_about, name="about"),
-    path('quiz_menu', views.render_quiz_menu, name="quiz_menu")
+    path('', include("app.urls")),
 ]
+
