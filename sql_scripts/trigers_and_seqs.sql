0e83e99a8e3d87662f7c2675f7ab03654b055e88
MariuszPaluch2001 <mariuszpaluch001@gmail.com>
2022-08-19 13:55:36 +0200

    Adjust fkntm_user_category trigger.

--- sql_scripts/trigers_and_seqs.sql
+++ sql_scripts/trigers_and_seqs.sql
@@ -49,14 +49,25 @@ $$;
 CREATE OR REPLACE TRIGGER fkntm_user_attempt BEFORE
     UPDATE OF user_id, user_category_id ON user_attempt
     EXECUTE PROCEDURE fkntm_user_attempt_function();
-/
 /*------------------------------------------------------------------------------------------*/
+
+CREATE FUNCTION fkntm_user_category_function()  
+   RETURNS trigger
+   LANGUAGE PLPGSQL
+AS 
+$$
+BEGIN
+	 RAISE EXCEPTION 'Non Transferable FK constraint  on table User_category is violated';
+	 
+	 RETURN NEW;
+END;
+$$;
+
 CREATE OR REPLACE TRIGGER fkntm_user_category BEFORE
     UPDATE OF category_id, user_id ON user_category
-BEGIN
-    raise_application_error(-20225, 'Non Transferable FK constraint  on table User_category is violated');
-END;
-/
+    EXECUTE PROCEDURE fkntm_user_category_function();
+    
+  
 /*------------------------------------------------------------------------------------------*/
 CREATE SEQUENCE card_sequence START WITH 1 NOCACHE ORDER;
 
