862bee6d57769f7926d7ac8fd87ac05eb0597541
MariuszPaluch2001 <mariuszpaluch001@gmail.com>
2022-09-21 10:09:05 +0200

    Add sample model.

--- app/models.py
+++ app/models.py
@@ -1,3 +1,7 @@
 from django.db import models
 
 # Create your models here.
+class User(models.Model):
+    login = models.CharField('user_login', max_length=30)
+    password_hash = models.CharField('user_pasword_hash', max_length=300)
+    creation_date = models.DateField('user_date_creation')
